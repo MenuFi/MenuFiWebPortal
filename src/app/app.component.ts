802a7adf85aa9857718221e9da61827c5b0a5964
Josh Wang <jwangc2@gmail.com>
2018-01-20 19:24:14 -0500

    Integrated basic Foundation 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { environment } from '../environments/environment';
+declare var $:any
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './shared/css/foundation.min.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'app';
-  isProduction = environment.production;
-  serverBaseUrl = environment.serverBaseUrl;
+export class AppComponent implements OnInit {
+  public title: string;
+  public isProduction: boolean;
+  public serverBaseUrl: string;
+  
+  constructor() {
+    this.title = 'MenuFi';
+    this.isProduction = environment.production;
+    this.serverBaseUrl = environment.serverBaseUrl;
+  }
+  
+  ngOnInit() {
+    $(document).foundation();
+  }
 }
